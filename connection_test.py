86535b217c0040b6c72b25951c47d736a2149472
enoch <ehsu@nautilusdigital.com>
2024-04-25 15:23:55 -0700

    removed unneeded files

--- connection_test.py
+++
@@ -1,8 +0,0 @@
-import sys
-import pokeapi
-
-data_to_pass_back = pokeapi.get_bst_from_pokemon(pokeapi.api_call("pokemon", "charizard"))
-input = sys.argv[1]
-output = data_to_pass_back
-print(output)
-sys.stdout.flush()
--- test.js
+++
@@ -1,7 +0,0 @@
-const spawner = require('child_process').spawn;
-const data_to_pass_in = 'abra';
-console.log('Data sent to python script:', data_to_pass_in);
-const python_process = spawner('python', ['connection_test.py', data_to_pass_in]);
-python_process.stdout.on('data', (data) => {
-    console.log('Data received from python script:', data.toString());
-});
